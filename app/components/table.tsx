65f6d8936b211f3fe57fa5b6efedbdb35c94875b
codal-matthew-fung <mfung@codal.com>
2026-02-02 11:23:49 +0000

    CIT-1063 Implement table with basic pagination

---
+++ app/components/table.tsx
@@ -0,0 +1,104 @@
+'use client';
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react";
+import { getData } from "../page";
+
+export interface Data {
+    metadata: Metadata
+    books: Book[]
+  }
+  
+  export interface Metadata {
+    total_count: number
+    total_pages: number
+    current_page: number
+    page_count: number
+  }
+  
+  export interface Book {
+    [k: string]: unknown
+    bookID: number
+    title: string
+    authors: string
+    average_rating: number
+    isbn: string
+    isbn13: number
+    language_code: string
+    num_pages: number
+    ratings_count: number
+    text_reviews_count: number
+    publication_date: string
+    publisher: string
+  }
+
+interface TableProps {
+    data: Data
+}
+export const Table = ({data}: TableProps) => {
+    const searchParams = useSearchParams();
+    const path = usePathname();
+    const router = useRouter();
+    const current_page = searchParams.get('page') ?? 1
+    
+    const handlePagination = ({current_page, next}: {current_page: string, next: boolean}) => {
+        let page = parseInt(current_page);
+        const newSearchParams = new URLSearchParams(searchParams)
+        if (next) {
+            page += 1;
+        } else {
+            page -= 1;
+        }
+
+        newSearchParams.set('page', page.toString())
+
+        router.replace(`${path}?${newSearchParams.toString()}`)
+    }
+
+    return (
+        <>
+        <div className="mb-20">
+          <p className="">Total Count: <span className="font-bold">{data.metadata.total_count}</span></p>
+          <p className="">Total Pages: <span className="font-bold">{data.metadata.total_pages}</span></p>
+          <p className="">Current Page: <span className="font-bold">{data.metadata.current_page}</span></p>
+          <ul className="inline-flex gap-2">
+          <li><button onClick={() =>handlePagination({'current_page': current_page.toString(), next: false})}>Previous Page</button></li>
+          <li><button onClick={() =>handlePagination({'current_page': current_page.toString(), next: true})}>Next Page</button></li>
+          </ul>
+        </div>
+        <table className="rounded-sm w-full max-w-full">
+          <thead>
+            <tr className="bg-zinc-100 border-b-zinc-900 border-zinc-600">
+              {
+                Object.keys(data?.books?.[0]).map((key) => (<th key={key} className={`text-md px-8 py-4 font-semibold text-zinc-600 align-bottom text-left ${key.includes('isbn') ? 'uppercase' : 'capitalize' }`}>{key.replaceAll('_', ' ')}</th>))
+              }
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.books.map((book, idx) => (
+                <tr key={book.isbn} className={`${idx % 2 != 0 && idx !== 0 ? 'bg-zinc-50' : ''}`}>
+                  {
+                    Object.keys(book).map((key) => {
+                        let value = book[key]
+                        if (key === 'authors') {
+                            const authors = book.authors.split('/')
+                            if (authors.length > 2) {
+                                value = authors[0] + ' et al' 
+                            } 
+                        }
+                        if (key === 'language_code') {
+                            value = book.language_code.toUpperCase()
+                        }
+                        return (
+                            <td key={`${book.isbn}-${key}`} className="px-8 py-4 text-zinc-500 font-medium">{value}</td>
+                        )
+                    })
+                  }
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        </>
+    )
+}
